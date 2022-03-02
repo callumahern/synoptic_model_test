141dd4b583f333505558c708111112e49bb330ba
Callum Ahern <callum.ahern@simplybusiness.co.uk>
2022-03-02 16:43:28 +0000

    added column to existing table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_02_163252) do
+ActiveRecord::Schema.define(version: 2022_03_02_164253) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2022_03_02_163252) do
     t.integer "author_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "genre"
     t.index ["author_id"], name: "index_books_on_author_id"
   end
 
