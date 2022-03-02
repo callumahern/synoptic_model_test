69964e7371bef8353fc83fd13977e8cf94e44e2c
Callum Ahern <callum.ahern@simplybusiness.co.uk>
2022-03-02 16:41:41 +0000

    created association / validatinos

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,5 @@
 class Author < ApplicationRecord
   has_many :books
+
+  validates :name, :age, presence: :true
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,5 @@
 class Book < ApplicationRecord
   belongs_to :author
+
+  validates :title, :year_released, :in_stock, presence: :true
 end
