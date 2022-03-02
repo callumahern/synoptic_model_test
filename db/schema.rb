b62711ab824b6373bf1666daf69c71c8705a7033
Callum Ahern <callum.ahern@simplybusiness.co.uk>
2022-03-02 16:44:49 +0000

    created devise model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_02_164253) do
+ActiveRecord::Schema.define(version: 2022_03_02_164430) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -30,6 +30,18 @@ ActiveRecord::Schema.define(version: 2022_03_02_164253) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "genre"
     t.index ["author_id"], name: "index_books_on_author_id"
+  end
+
+  create_table "models", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_models_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
   end
 
 end
